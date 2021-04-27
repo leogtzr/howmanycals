a60259f25eca01f6bdf2fca7b956baa0c527c3b6
leogtzr <leogutierrezramirez@gmail.com>
2021-04-27 17:53:51 -0600

    fix compose file to persist DB info if it is shutdown ...

--- db/sql/db.sql
+++ db/sql/db.sql
@@ -6,16 +6,16 @@ CREATE TABLE IF NOT EXISTS product (
 );
 
 -- Creation of order_status table
-CREATE TABLE IF NOT EXISTS order_status (
-  order_status_id varchar(200) NOT NULL,
-  update_at TIMESTAMP,
-  sale_id varchar(200) NOT NULL,
-  status_name_id INT NOT NULL,
-  PRIMARY KEY (order_status_id),
-  CONSTRAINT fk_sale
-      FOREIGN KEY(sale_id)
-    REFERENCES sale(sale_id),
-  CONSTRAINT fk_status_name
-      FOREIGN KEY(status_name_id)
-    REFERENCES status_name(status_name_id)
-);
+-- CREATE TABLE IF NOT EXISTS order_status (
+--   order_status_id varchar(200) NOT NULL,
+--   update_at TIMESTAMP,
+--   sale_id varchar(200) NOT NULL,
+--   status_name_id INT NOT NULL,
+--   PRIMARY KEY (order_status_id),
+--   CONSTRAINT fk_sale
+--       FOREIGN KEY(sale_id)
+--     REFERENCES sale(sale_id),
+--   CONSTRAINT fk_status_name
+--       FOREIGN KEY(status_name_id)
+--     REFERENCES status_name(status_name_id)
+-- );
