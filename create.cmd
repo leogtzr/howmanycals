0521d5371e213b204ffa36e5d7ddf5417c2a7c7a
Leo Gutierrez <Leonardo.Gutierrez@autozone.com>
2021-11-07 16:48:04 -0700

    add scripts to build and run jar file under Win

---
+++ create.cmd
@@ -0,0 +1,5 @@
+@echo off
+
+ant -file "build.xml" -Dnb.internal.action.name=rebuild clean jar
+
+exit /b 0
---
+++ run.cmd
@@ -0,0 +1,6 @@
+@echo off
+
+java -XX:+PrintCommandLineFlags -XX:+PrintClassHistogram -Dlogback.configurationFile=logback.xml --enable-preview ^
+	-jar "dist/howmanycals.jar" "muscle_gain.properties"
+
+exit /b 0
